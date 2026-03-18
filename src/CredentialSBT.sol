6f5c2bf2996d5db8163f4e59f7c9604b6760ecfb
ruslan tasmukhanov <ruslahn.q@gmail.com>
2026-03-18 19:23:01 +0500

    add issuing & verifying & revoking the credentials with soulbound constraint

--- src/CredentialSBT.sol
+++ src/CredentialSBT.sol
@@ -13,7 +13,7 @@ contract CredentialSBT is ERC721, Ownable {
         return true;
     }
 
-    function f(bytes4 interfaceId) public view virtual override returns (bool) {
+    function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
         // choosing ERC-5192 support as this is a soulbound token contract
         return interfaceId == 0xb45a3c0e || super.supportsInterface(interfaceId);
     }
@@ -39,5 +39,49 @@ contract CredentialSBT is ERC721, Ownable {
     constructor(string memory _institutionName) ERC721("Academic Credential", "CRED") Ownable(msg.sender) {
         institutionName = _institutionName;
         _tokenIdCounter = 1;
+    }
+
+    function issueCredential(address student, string memory metadataUri, bytes32 metadataHash) external onlyOwner returns (uint256) {
+        require(student != address(0), "Invalid student address");
+        require(bytes(metadataUri).length > 0, "Metadata URI cannot be empty");
+        require(metadataHash != bytes32(0), "Metadata hash cannot be empty");
+
+        uint256 tokenId = _tokenIdCounter++;
+        _safeMint(student, tokenId);
+
+        _credentials[tokenId] = Credential(metadataUri, metadataHash, block.timestamp, true, false);
+        _studentTokens[student].push(tokenId);
+
+        emit CredentialIssued(student, tokenId, metadataUri, metadataHash, block.timestamp);
+        emit Locked(tokenId);
+
+        return tokenId;
+    }
+
+    function revokeCredential(uint256 tokenId) external onlyOwner {
+        require(_credentials[tokenId].exists, "Credential does not exist");
+        require(!_credentials[tokenId].isRevoked, "Credential already revoked");
+
+        address student = ownerOf(tokenId);
+        _credentials[tokenId].isRevoked = true;
+        _burn(tokenId);
+
+        emit CredentialRevoked(student, tokenId, block.timestamp);
+    }
+
+    function verifyCredential(address student, uint256 tokenId) external view returns (bool valid, bool revoked, string memory uri, bytes32 hash) {
+        Credential memory cred = _credentials[tokenId];
+        require(cred.exists, "Credential does not exist");
+
+        revoked = cred.isRevoked;
+        uri     = cred.metadataUri;
+        hash    = cred.metadataHash;
+        valid   = !revoked && ownerOf(tokenId) == student;
+    }
+
+    function _update(address to, uint256 tokenId, address auth) internal virtual override returns (address) {
+        address from = _ownerOf(tokenId);
+        require(from == address(0) || to == address(0), "Soulbound: token is non-transferable");
+        return super._update(to, tokenId, auth);
     }
 }
